4aacc9d7b60aff90dac9cfc45788e220602071a2
Ana Beatriz <anabeatrizcorreiaribeiro010@gmail.com>
2024-08-23 08:30:24 -0300

    api2

--- app/Models/Competitor.php
+++ app/Models/Competitor.php
@@ -5,7 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class Competitor extends Model
+class competitor extends Model
 {
     use HasFactory;
 
